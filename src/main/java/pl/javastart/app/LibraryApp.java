c287620047ef42b72b182d8f5e7241fc79f3963a
favael <rafaldogoda86@gmail.com>
2020-06-10 15:48:53 +0200

    dodanie abstrakcji w Pubblication, Enum przeniesiony do klasy LibraryControl

--- src/main/java/pl/javastart/app/LibraryApp.java
+++ src/main/java/pl/javastart/app/LibraryApp.java
@@ -2,7 +2,7 @@ package pl.javastart.app;
 
 public class LibraryApp {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v1.4";
+        final String appName = "Biblioteka v1.6";
         System.out.println (appName);
         LibraryControl libraryControl = new LibraryControl ();
         libraryControl.controlLoop ();
--- src/main/java/pl/javastart/app/LibraryControl.java
+++ src/main/java/pl/javastart/app/LibraryControl.java
@@ -108,5 +108,40 @@ class LibraryControl {
             consolePrinter.printLine (option.toString ());
         }
     }
+    private enum Option {
+        EXIT (0, "wyjście z programu"),
+        ADD_BOOK (1, "dodanie nowej ksiazki"),
+        ADD_MAGAZINE (2, "dodanie nowego magazynu"),
+        PRINT_BOOKS (3, "wyswietl dostepne ksiazki"),
+        PRINT_MAGAZINES (4, "wyswietl dostepne magazyny");
 
+        private final int value;
+        private final String description;
+
+        Option(int value, String description) {
+            this.value = value;
+            this.description = description;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public String toString() {
+            return value + " - " + description;
+        }
+
+        static Option createFromInt(int option) throws NoSuchOptionException {
+            try {
+                return Option.values ()[option];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new NoSuchOptionException ("Brak opcji o id " + option);
+            }
+        }
+    }
 }
